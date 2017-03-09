6a78eb8427f916da064f7a93607af02f62129bd1
Andres Vergel <andres_vergel02@hotmail.com>
2017-03-09 17:38:29 -0500

    fixed bug console.log

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // Integracion de animate.css con jquery
 $.fn.extend({
   animateCss: function (animationName) {
-    console.log(animationName)
     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
     this.addClass('animated ' + animationName).one(animationEnd, function() {
         $(this).removeClass('animated ' + animationName);
@@ -16,6 +15,5 @@ var animation = ['bounce', 'rubberBand', 'pulse', 'shake']
 logo.click(function(){
   var max = animation.length
   var num = Math.floor(Math.random() * (max - 0))
-  console.log(num)
   logo.animateCss(animation[num])
 })
