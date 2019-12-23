2069dc9094182afa381e989c9093b9abe7a085e6
Andres Vergel Alvarez <amvergel@hotmail.com>
2019-12-23 15:08:10 -0500

    refactoring styles

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 
-// Integracion de animate.css con jquery
-$.fn.extend({
-  animateCss: function (animationName) {
-    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    this.addClass('animated ' + animationName).one(animationEnd, function() {
-        $(this).removeClass('animated ' + animationName);
-    });
-  }
-});
+const logo = document.getElementById('logo')
 
-// Evento click en logo
-var logo = $('#logo')
-var animation = ['bounce', 'rubberBand', 'pulse', 'shake']
-logo.click(function(){
-  var max = animation.length
-  var num = Math.floor(Math.random() * (max - 0))
-  logo.animateCss(animation[num])
-})
+logo.addEventListener('click', hdlClick)
+
+function hdlClick(e){
+    e.target.classList.add('animated', 'bounce')
+    e.target.addEventListener('animationend', hdlAnimationEnd)
+
+    function hdlAnimationEnd(e){
+        e.target.classList.remove('animated', 'bounce')
+        e.target.removeEventListener('animationend', hdlAnimationEnd)
+    }
+}
